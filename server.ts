c64522ff6d3cc83233d6f7793211c57c348beca8
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2024-02-22 18:00:17 +0100

    Update types

--- server.ts
+++ server.ts
@@ -33,7 +33,7 @@ export default {
       /*
        * Open a cache instance in the worker and a custom session instance.
        */
-      if (!env?.SESSION_SECRET) {
+      if (!env.SESSION_SECRET) {
         throw new Error('SESSION_SECRET environment variable is not set');
       }
 

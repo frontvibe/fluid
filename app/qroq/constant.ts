0a07d29eb720b022afce89869cace138ff478438
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2025-03-14 17:30:00 -0400

    add groq constant

---
+++ app/qroq/constant.ts
@@ -0,0 +1,15 @@
+export const contentPositionValues = [
+  'top_left',
+  'top_center',
+  'top_right',
+  'middle_left',
+  'middle_center',
+  'middle_right',
+  'bottom_left',
+  'bottom_center',
+  'bottom_right',
+] as const;
+
+export const contentAlignmentValues = ['left', 'center', 'right'] as const;
+
+export const aspectRatioValues = ['square', 'video', 'auto'] as const;

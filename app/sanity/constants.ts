d46db5b961ae18ba5a8f2c6f341bbaa421786879
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2025-03-15 20:33:05 -0400

    update sanity presentation path

--- app/sanity/constants.ts
+++ app/sanity/constants.ts
@@ -1,3 +1,3 @@
 export const SANITY_API_VERSION = '2023-03-20';
-export const SANITY_STUDIO_PREVIEW_URL = '/sanity/preview';
+export const SANITY_STUDIO_PREVIEW_URL = '/sanity-preview';
 export const SANITY_STUDIO_URL = '/cms';

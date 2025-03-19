b9a48bd3c8c9227bb45bcb9049f16b616df88ebf
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2025-03-19 19:28:29 -0400

    Update queries

---
+++ app/data/sanity/footers.ts
@@ -0,0 +1,22 @@
+import {defineQuery} from 'groq';
+
+import {COLOR_SCHEME_FRAGMENT} from './fragments';
+import {getIntValue} from './utils';
+
+export const FOOTER_SETTINGS_FRAGMENT = defineQuery(`{
+  colorScheme -> ${COLOR_SCHEME_FRAGMENT},
+  customCss,
+  hide,
+  padding
+}`);
+
+export const FOOTER_SOCIAL_LINKS_ONLY_FRAGMENT = defineQuery(`{
+  _key,
+  _type,
+  "copyright": ${getIntValue('copyright')},
+  "settings": ${FOOTER_SETTINGS_FRAGMENT},
+}`);
+
+export const FOOTERS_FRAGMENT = defineQuery(`{
+  _type == 'socialLinksOnly' => ${FOOTER_SOCIAL_LINKS_ONLY_FRAGMENT},
+}`);
--- app/data/sanity/queries.ts
+++ app/data/sanity/queries.ts
@@ -1,11 +1,32 @@
 import {defineQuery} from 'groq';
 
+import {FOOTERS_FRAGMENT} from './footers';
 import {
   COLOR_SCHEME_FRAGMENT,
   HEADER_FRAGMENT,
   SETTINGS_FRAGMENT,
   THEME_CONTENT_FRAGMENT,
 } from './fragments';
+import {
+  COLLECTION_SECTIONS_FRAGMENT,
+  PRODUCT_SECTIONS_FRAGMENT,
+  SECTIONS_FRAGMENT,
+} from './sections';
+import {getIntValue} from './utils';
+
+export const DEFAULT_PRODUCT_TEMPLATE =
+  defineQuery(`*[_type == 'productTemplate' && default == true][0] {
+    _type,
+    name,
+    sections[] ${PRODUCT_SECTIONS_FRAGMENT},
+  }`);
+
+export const DEFAULT_COLLECTION_TEMPLATE =
+  defineQuery(`*[_type == 'collectionTemplate' && default == true][0] {
+    _type,
+    name,
+    sections[] ${COLLECTION_SECTIONS_FRAGMENT},
+  }`);
 
 export const ROOT_QUERY = defineQuery(`{
   '_type': 'root',
@@ -15,7 +36,59 @@ export const ROOT_QUERY = defineQuery(`{
     extra,
     heading,
   },
+  "footer": *[_type == 'footer'][0] {
+    "footer": footers[] ${FOOTERS_FRAGMENT},
+    sections[] ${SECTIONS_FRAGMENT},
+  },
   "header": *[_type == "header"][0] ${HEADER_FRAGMENT},
   "settings": *[_type == "settings"][0] ${SETTINGS_FRAGMENT},
   "themeContent": *[_type == "themeContent"][0] ${THEME_CONTENT_FRAGMENT},
+}`);
+
+export const COLLECTION_QUERY = defineQuery(`{
+  _type,
+  "collection": *[_type == "collection" && store.slug.current == $collectionHandle][0] {
+    store {
+      gid,
+    },
+    template -> {
+      sections[] ${SECTIONS_FRAGMENT},
+    },
+  },
+  "defaultCollectionTemplate": ${DEFAULT_COLLECTION_TEMPLATE},
+}`);
+
+export const PAGE_QUERY =
+  defineQuery(`*[(_type == "page" && ($handle != "home" && slug.current == $handle)) || (
+    _type == "home" && $handle == "home"
+  )][0] {
+    _type,
+    sections[] ${SECTIONS_FRAGMENT},
+    seo {
+      "title": ${getIntValue('title')},
+      "description": ${getIntValue('description')},
+      image,
+    },
+  }`);
+
+export const PRODUCT_QUERY = defineQuery(`{
+  "_type": "product",
+  'product': *[_type == "product" && store.slug.current == $productHandle][0] {
+    store {
+      gid,
+    },
+    template -> {
+      sections[] ${PRODUCT_SECTIONS_FRAGMENT},
+    },
+  },
+  "defaultProductTemplate": ${DEFAULT_PRODUCT_TEMPLATE},
+}`);
+
+/**
+ * Used by Typegen to generate the ALL_SECTIONS_QUERYResult type
+ */
+export const ALL_SECTIONS_QUERY = defineQuery(`*[][0] {
+  sections[] ${SECTIONS_FRAGMENT},
+  productSections[] ${PRODUCT_SECTIONS_FRAGMENT},
+  collectionSections[] ${COLLECTION_SECTIONS_FRAGMENT},
 }`);

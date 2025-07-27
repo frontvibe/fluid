7ce1c1314031d87a8971607d5d24816f49657121
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2025-07-27 14:59:55 -0400

    Update prettier config (#517)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,13 @@
-module.exports = {
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
   arrowParens: 'always',
   bracketSpacing: false,
   plugins: ['prettier-plugin-tailwindcss'],
   singleQuote: true,
   trailingComma: 'all',
 };
+
+export default config;

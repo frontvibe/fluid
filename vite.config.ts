12e7b2d7cbed08fb76aca5aebc7b4414d4092b13
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2025-12-01 15:38:05 -0500

    Update typegen

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import {oxygen} from '@shopify/mini-oxygen/vite';
 import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 import {typegenWatcher} from './types/plugin';
 
 export default defineConfig({
